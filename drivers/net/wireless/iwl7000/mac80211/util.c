29304b81b5767c4463814ab977932308e7bfcabb
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-06-05 11:33:49 +0300

    CHROMIUM: iwl7000: mac80211: add the virtual monitor after reconfig complete
    In reconfig we add the virtual monitor in 2 cases:
    1. If we are resuming (it was deleted on suspend)
    2. If it was added after an error but before the reconfig
       (due to the last non-monitor interface removal).
    
    In the second case, the removal of the non-monitor interface will succeed
    but the addition of the virtual monitor will fail, so we add it in the
    reconfig.
    
    The problem is that we mislead the driver to think that this is an existing
    interface that is getting re-added - while it is actually a completely new
    interface from the drivers' point of view.
    
    Some drivers act differently when a interface is re-added. For example, it
    might not initialize things because they were already initialized.
    Such drivers will - in this case - be left with a partialy initialized vif.
    
    To fix it, add the virtual monitor after reconfig_complete, so the
    driver will know that this is a completely new interface.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I2e37b68375278987e303d6c00cc5f3d8334d2f96
    iwl7000-tree: 5f03f3c4cbc3a9db9de7ae822814f503b94add57

--- drivers/net/wireless/iwl7000/mac80211/util.c
+++ drivers/net/wireless/iwl7000/mac80211/util.c
@@ -2154,10 +2154,6 @@ int ieee80211_reconfig(struct ieee80211_local *local)
 		cfg80211_sched_scan_stopped_locked(local->hw.wiphy, 0);
 
  wake_up:
-
-	if (local->monitors == local->open_count && local->monitors > 0)
-		ieee80211_add_virtual_monitor(local);
-
 	/*
 	 * Clear the WLAN_STA_BLOCK_BA flag so new aggregation
 	 * sessions can be established after a resume.
@@ -2210,6 +2206,9 @@ int ieee80211_reconfig(struct ieee80211_local *local)
 				ieee80211_sta_restart(sdata);
 		}
 	}
+
+	if (local->monitors == local->open_count && local->monitors > 0)
+		ieee80211_add_virtual_monitor(local);
 
 	if (!suspended)
 		return 0;
